6e096ae53efc4cfe3238312545b0e3fb19e6334f
= <=>
2023-10-01 21:18:43 -0500

    While

---
+++ python_1.py
@@ -0,0 +1,7 @@
+nombre = "David Gomez"
+cel = 31077288966
+correo = "david@ejemplo.com.ex"
+
+print(nombre)
+print(cel)
+print(correo)
---
+++ python_2.py
@@ -0,0 +1,23 @@
+'''
+numero = int(input("Ingrese un numero mayor a 15: "))
+
+while numero <= 15:
+    print(f"El numero ingresado fue: {numero}")
+    numero = int(input("Ingrese un numero mayor a 15: "))
+
+print("El numero proporcionado es mayor a 15, Excelente")
+'''
+
+print("Adivine el numero\ndel 1 al 15\n  ¡Suerte!")
+numero_2 = int(input("Digite numero: "))
+encontrar_num = int(8)
+
+while True:
+    print(f"El numero ingresado fue: {numero_2}")
+    if numero_2 == encontrar_num:
+        print("Felicidades encontro el numero escondido")
+        break
+    else:
+        print("Siga intentando")
+        numero_2 = int(input("Digite numero: "))
+print("Esperamos que lo haya dosfrutado")

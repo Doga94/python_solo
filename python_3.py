8c23225422ea8dd4c804f4b5025c4e143ba25444
= <=>
2023-10-01 22:49:08 -0500

    Listas con mas practica

--- python_3.py
+++ python_3.py
@@ -21,9 +21,27 @@ print(f"Quedo ordenada: {lista}")
 print(f"Vamos a desordenar la lista: {lista} va a salir ordenada asi: reverse.sort()")
 lista.reverse()
 print(f"Quedo ordenada: {lista}")
-'''
+
 
 
 lista_2 = [1, 2, 3, 4, 5]
 lista_par = list(filter(lambda x: x % 2 == 0, lista_2))
 print(lista_par)
+
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8]
+lista.sort()
+
+print(f"Vamos a sacar de esta lista {lista} los numeros que sean divisibles en 3")
+for x in lista:
+    if x % 3 == 0:
+        print(x)
+'''
+print("Crearemos un rango de 2 a 6")
+rango = range(2,7)
+for x_1 in rango:
+    print(x_1)
+
+print("Imprimiremos un rango de 2 a 10 de 2 en 2:")
+for i in range(2, 11, 2):
+    print(i)
